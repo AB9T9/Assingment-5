b5cd751fa14096c3318d8cf1686c2064fb4a08c9
AB SIDDIK <abft102ctg@gmai.com>
2025-08-28 23:47:46 +0600

    coin part almost done

--- js-file/script.js
+++ js-file/script.js
@@ -11,3 +11,27 @@ for (const heart of heartButton) {
     heartText.innerText = heartCount;
   });
 }
+//! working with calling coing ;
+const coinText = document.getElementById("coin");
+let coinCount = parseInt(coinText.innerText);
+console.log(coinCount);
+const callButton = document.getElementsByClassName("call-button");
+for (const call of callButton) {
+  call.addEventListener("click", function (e) {
+    e.preventDefault();
+    const serviceName =
+      call.parentElement.parentElement.querySelector("h1").innerText;
+    const serviceNumber =
+      call.parentElement.parentElement.querySelector("h2").innerText;
+
+    if (coinCount > 0) {
+      coinCount = coinCount - 20;
+      alert("Calling " + serviceName + serviceNumber);
+      document.getElementById("coin").innerText = coinCount;
+    } else {
+      alert("You have no sufficient balance,Mininum 20 coin required for call");
+    }
+
+    console.log("i am click by call function ");
+  });
+}
